542e98e8e5617a183bb5c861a9d25bd76988e8f8
gultaking <57599732+gultaking@users.noreply.github.com>
2023-06-29 23:01:35 +0400

    Enter a day of the week and print the corresponding day name (#15)
    Enter a day of the week and print the corresponding day name

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
 ﻿using CSharpFundamentals.Lesson3;
 
-//Task1 task1 = new Task1();
-//task1.PrintEvenOrOdd();
-
-//Task2 task2 = new Task2();
-//task2.PrintAgeTests();
-
-Task3 task3 = new Task3();
-task3.CalculatingLeapYearTests();
+Task4 task4 = new Task4();
+task4.WeekDaysTests();
