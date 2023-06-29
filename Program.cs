704b82cb7fb046283c764f68aa309626829cf39b
gultaking <gultakin.beydullayeva@gmail.com>
2023-06-29 20:04:52 +0400

    Enter a day of the week and print the corresponding day name

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using CSharpFundamentals.Lesson3;
 
-Task2 task2 = new Task2();
-task2.PrintAgeTests();
+//Task2 task2 = new Task2();
+//task2.PrintAgeTests();
+
+Task4 task4 = new Task4();
+task4.WeekDaysTests();
