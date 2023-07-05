7d3f2d3dca7fea160d1e3f28baf721940c101055
gultaking <gultakin.beydullayeva@gmail.com>
2023-07-05 16:08:56 +0400

    To display the number of days in that month by month number

--- Lesson3/Task8.cs
+++ Lesson3/Task8.cs
@@ -4,27 +4,33 @@ public class Task8
 {
     public int DaysOfMonth(int month)
     {
-        int dayCount = month;
+        int dayCount = 1;
 
         if (month < 1 || month > 12)
         {
             throw new Exception("Month cannot be less than 1 or more than 12!");
         }
 
-        switch (dayCount)
+        switch (month)
         {
-            case 1: return 31;
-            case 2: return 28;
-            case 3: return 31;
-            case 4: return 30;
-            case 5: return 31;
-            case 6: return 30;
-            case 7: return 31;
-            case 8: return 30;
-            case 9: return 31;
-            case 10: return 31;
-            case 11: return 30;
-            case 12: return 31;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                dayCount = 31;
+                break;
+            case 2:
+                dayCount = 28;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                dayCount = 30;
+                break;
         }
 
         return dayCount;
