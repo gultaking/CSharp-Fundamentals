1aefc666ad57a71751ac3deab1fe677c758af7ea
gultaking <57599732+gultaking@users.noreply.github.com>
2023-07-05 16:51:16 +0400

    To display the number of days in that month by month number (#19)
    * To display the number of days in that month by month number

---
+++ Lesson3/Task8.cs
@@ -0,0 +1,51 @@
+namespace CSharpFundamentals.Lesson3;
+
+public class Task8
+{
+    public int GetDayCountOfMonth(int month)
+    {
+        int dayCount;
+
+        switch (month)
+        {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                dayCount = 31;
+                break;
+            case 2:
+                dayCount = 28;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                dayCount = 30;
+                break;
+            default:
+                throw new Exception("Month cannot be less than 1 or more than 12");
+        }
+
+        return dayCount;
+    }
+
+    public void GetDayCountOfMonthTests()
+    {
+        try
+        {
+            Console.Write("Enter month number: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            int dayCount = GetDayCountOfMonth(month);
+            Console.WriteLine("There are {0} days in the {1}st month", dayCount, month);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿using CSharpFundamentals.Lesson3;
 
-Task7 task7 = new Task7();
-task7.FindCharacterTest();
+Task8 task8 = new Task8();
+task8.GetDayCountOfMonthTests();
