97535e622f7ee789a9efc63cc54fd06711b8a406
gultaking <gultakin.beydullayeva@gmail.com>
2023-07-07 00:04:16 +0400

    Finding the type of triangle(equilateral, isosceles or scalene)

--- Lesson3/Task10.cs
+++ Lesson3/Task10.cs
@@ -9,17 +9,22 @@ public class Task10
         Scalene
     }
 
-    public object GetTypeTriangle(int sideA, int sideB, int sideC)
+    public string GetTypeTriangle(int side1, int side2, int side3)
     {
-        TypeTriangle typeTriangleA = TypeTriangle.Equilateral;
-        TypeTriangle typeTriangleB = TypeTriangle.Isosceles;
-        TypeTriangle typeTriangleC = TypeTriangle.Scalene;
+        string typeTriangleA = TypeTriangle.Equilateral.ToString();
+        string typeTriangleB = TypeTriangle.Isosceles.ToString();
+        string typeTriangleC = TypeTriangle.Scalene.ToString();
 
-        if (sideA == sideB && sideA == sideC && sideB == sideC)
+        if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+        {
+            throw new Exception("The sides of a triangle cannot be negative!");
+        }
+
+        if (side1 == side2 && side1 == side3 && side2 == side3)
         {
             return typeTriangleA;
         }
-        else if (sideA == sideB || sideB == sideC)
+        else if (side1 == side2 || side2 == side3 || side1 == side3)
         {
             return typeTriangleB;
         }
@@ -29,17 +34,24 @@ public class Task10
 
     public void GetTypeTriangleTests()
     {
-        Console.Write("Enter side a triangle: ");
-        int sideA = Convert.ToInt32(Console.ReadLine());
+        try
+        {
+            Console.Write("Enter side a triangle: ");
+            int sideA = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter side b triangle: ");
-        int sideB = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter side b triangle: ");
+            int sideB = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter side c triangle: ");
-        int sideC = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter side c triangle: ");
+            int sideC = Convert.ToInt32(Console.ReadLine());
 
-        object result = GetTypeTriangle(sideA, sideB, sideC);
+            object result = GetTypeTriangle(sideA, sideB, sideC);
 
-        Console.WriteLine(result);
+            Console.WriteLine("This triangle is an {0} triangle ",  result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
