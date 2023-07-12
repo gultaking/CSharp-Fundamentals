bf776d46721de7033f4180baca398676937b4bcc
gultaking <57599732+gultaking@users.noreply.github.com>
2023-07-12 15:27:20 +0400

    Inserting and printing array elements (#22)
    * Inserting and printing array elements

---
+++ Lesson4/Task1.cs
@@ -0,0 +1,34 @@
+namespace CSharpFundamentals.Lesson4;
+
+public class Task1
+{
+    public void PrintArrayElementsTests()
+    {
+        Console.Write("Enter the size of the arrays elements: ");
+        int sizeOfElements = Convert.ToInt32(Console.ReadLine());
+
+        if (sizeOfElements < 0)
+        {
+            throw new Exception("The size of array elements cannot be negative!");
+        }
+
+        double[] arrayElements = new double [sizeOfElements];
+
+        for (int i = 0; i < arrayElements.Length; i++)
+        {
+            Console.Write("Insert the elements of the array {0}: ", i);
+            arrayElements[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        PrintArrayElements(arrayElements);
+    }
+    public void PrintArrayElements(double[] arrayElements)
+    {
+        Console.Write("Elements of an array: ");
+        
+        for (int i = 0; i < arrayElements.Length; i++)
+        {
+            Console.Write("\n " + arrayElements[i]);
+        }
+    }
+}
