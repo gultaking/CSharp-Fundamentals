2a5c95a0195de3a7a34974aab0676e28761c2c57
gultaking <57599732+gultaking@users.noreply.github.com>
2023-07-20 14:38:44 +0400

    CSF-4-9: Find the minimum and maximum element from an array (#29)
    * CSF-4-9: Find the minimum and maximum element in an array

---
+++ Lesson4/Task9.cs
@@ -0,0 +1,59 @@
+namespace CSharpFundamentals.Lesson4;
+
+public class Task9
+{
+    public void FindMinMaxElementTests()
+    {
+        Console.Write("Enter length the array: ");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        if (length <= 0)
+        {
+            throw new Exception("The size of array elements cannot be negative or zero!");
+        }
+        
+        double[] array = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("Enter array ({0}): ", i);
+            array[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        double minimumElement = FindMinElement(array);
+        Console.Write("The minimum element of the array: " + minimumElement);
+
+        double maximumElement = FindMaxElement(array);
+        Console.Write("\nThe maximum element of the array: " + maximumElement);
+    }
+
+    double FindMinElement(double[] array)
+    {
+        double minimum = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < minimum)
+            {
+                minimum = array[i];
+            }
+        }
+
+        return minimum;
+    }
+
+    double FindMaxElement(double[] array)
+    {
+        double maximum = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > maximum)
+            {
+                maximum = array[i];
+            }
+        }
+
+        return maximum;
+    }
+}
