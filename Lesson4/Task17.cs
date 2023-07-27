1ffc9164d6be45d1b0989d4f9a782a2a9e810d33
gultaking <57599732+gultaking@users.noreply.github.com>
2023-07-27 09:11:00 +0400

    CSF-4-17: To calculate the sum of two matrices of the same size (#37)

---
+++ Lesson4/Task17.cs
@@ -0,0 +1,108 @@
+namespace CSharpFundamentals.Lesson4;
+
+public class Task17
+{
+    public void SumOfArraysTests()
+    {
+        try
+        {
+            Console.Write("Enter rows size: ");
+            int rowSize = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter columns size: ");
+            int columnSize = Convert.ToInt32(Console.ReadLine());
+
+            if (rowSize <= 0 || columnSize <= 0)
+            {
+                throw new Exception("The row and column size of an array cannot be zero or negative!");
+            }
+
+            if (rowSize == 1 && columnSize == 1)
+            {
+                throw new Exception("It is not a two-dimensional array!");
+            }
+
+            Console.WriteLine("Enter the first array: ");
+            int[,] array1 = InputArray(rowSize, columnSize);
+
+            Console.WriteLine("Enter the second array: ");
+            int[,] array2 = InputArray(rowSize, columnSize);
+
+            Console.WriteLine("Entered first array: ");
+            Print(array1);
+
+            Console.WriteLine("Entered second array: ");
+            Print(array2);
+
+            Console.WriteLine("The sum of the two arrays: ");
+            int[,] sumArray = SumOfArrays(array1, array2);
+            Print(sumArray);
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    int[,] InputArray(int rowSize, int columnSize)
+    {
+        int[,] array = new int[rowSize, columnSize];
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write("Array({0}, {1}): ", i, j);
+                array[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        return array;
+    }
+
+    int[,] SumOfArrays(int[,] array1, int[,] array2)
+    {
+        if (array1.GetLength(0) != array2.GetLength(0))
+        {
+            throw new Exception("The row sizes of the arrays are not same!");
+        }
+
+        if (array1.GetLength(1) != array2.GetLength(1))
+        {
+            throw new Exception("The column sizes of the arrays are not same!");
+        }
+
+        int[,] sumArray = new int[array1.GetLength(0), array1.GetLength(1)];
+
+        for (int i = 0; i < array1.GetLength(0); i++)
+        {
+            for (int j = 0; j < array1.GetLength(1); j++)
+            {
+                sumArray[i, j] = array1[i, j] + array2[i, j];
+            }
+        }
+
+        return sumArray;
+    }
+
+    void Print(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (array.GetLength(1) - 1 == j)
+                {
+                    Console.Write(array[i, j]);
+                }
+                else
+                {
+                    Console.Write(array[i, j] + ", ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
