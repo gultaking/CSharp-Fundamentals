b94eb6d4b4d30db4b2613173c71c85d5edf5e99a
gultaking <gultakin.beydullayeva@gmail.com>
2023-10-19 15:05:41 +0400

    Use foreach loops to iterate over the elements of a collection

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using CSharpFundamentals.Lesson6;
 
-Task2 task2 = new Task2();
-task2.Add();
-task2.AddRange();
-task2.Push();
-task2.Enqueue();
+Task5 task5 = new Task5();
+task5.IterationForeachOverDictionary();
+task5.IterationForeachOverList();
