471373918bac9f84642284ecdec7f9c81649dbb2
gultaking <gultakin.beydullayeva@gmail.com>
2023-10-19 14:30:25 +0400

    CSF-6-4: Remove elements using methods like Remove, RemoveAt, Dequeue, or Pop.

--- Lesson6/Task4.cs
+++ Lesson6/Task4.cs
@@ -2,29 +2,157 @@ namespace CSharpFundamentals.Lesson6;
 
 public class Task4
 {
-    public void RemoveElementInList()
+    public void Remove()
     {
-        Console.Write("Enter the size of the List: ");
-        int size = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Enter elements of the List");
-        List<string> myList = new List<string>();
-
-        for (int i = 0; i < size; i++)
+        try
         {
-            Console.Write("Enter element {0}: ", i);
-            myList.Add(Console.ReadLine());
+            Console.Write("Enter the size of the List: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            if (size == 0 || size < 0)
+            {
+                throw new Exception("Size cannot be zero or negative!");
+            }
+            
+            Console.WriteLine("Enter elements of the List");
+            List<string> myList = new List<string>();
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Enter element {0}: ", i);
+                myList.Add(Console.ReadLine());
+            }
+
+            Console.Write("Enter the element to delete: ");
+            string element = Console.ReadLine();
+
+            if (element == null || !myList.Contains(element))
+            {
+                throw new Exception("Element cannot be empty or element not found in List!");
+            }
+
+            if (myList.Contains(element))
+            {
+                if (myList.Remove(element))
+                {
+                    Console.WriteLine($"Element is removed: {element}");
+                }
+                else
+                {
+                    Console.WriteLine($"Element is not removed: {element}");
+                }
+            }
+
+            Console.WriteLine("Element of the List");
+            foreach (string item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public void RemoveAt()
+    {
+        try
+        {
+            Console.Write("Enter the size of the List: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            if (size == 0 || size < 0)
+            {
+                throw new Exception("Size cannot be zero or negative!");
+            }
+
+            Console.WriteLine("Enter elements of the List");
+            List<int> myList = new List<int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Enter element {0}: ", i);
+                myList.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            Console.Write("Enter index: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            if (index > myList.Count || index < 0)
+            {
+                throw new Exception("Index cannot be less than zero or greater than Count");
+            }
+
+            myList.RemoveAt(index);
+
+            Console.WriteLine("Element of the List");
+            foreach (int item in myList)
+            {
+                Console.WriteLine(item);
+            }
         }
 
-        
-        Console.Write("Enter the element to delete: ");
-        string element = Console.ReadLine();
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
 
-        Console.WriteLine(element != null && myList.Remove(element) ? "Element is removed!" : "Element is not removed!");
+    public void Dequeue()
+    {
+        Console.Write("Enter the size of Queue: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        
+        if (size == 0 || size < 0)
+        {
+            throw new Exception("Size cannot be zero or negative!");
+        }
+        
+        Console.WriteLine("Enter the element of the Queue");
+        Queue<int> queue = new Queue<int>();
 
         for (int i = 0; i < size; i++)
         {
-            Console.WriteLine(myList[i]);
+            Console.Write("Enter element: ");
+            queue.Enqueue(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        queue.Dequeue();
+
+        Console.WriteLine("Elements of the Queue");
+        foreach (int element in queue)
+        {
+            Console.WriteLine(element);
+        }
+    }
+
+    public void Pop()
+    {
+        Console.Write("Enter the size of Stack: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        
+        if (size == 0 || size < 0)
+        {
+            throw new Exception("Size cannot be zero or negative!");
+        }
+        
+        Console.WriteLine("Enter the element of the Stack");
+        Stack<double> stack = new Stack<double>();
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write("Enter element: ");
+            stack.Push(Convert.ToDouble(Console.ReadLine()));
+        }
+
+        stack.Pop();
+        
+        Console.WriteLine("Element of the Stack");
+        foreach (double element in stack)
+        {
+            Console.WriteLine(element);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using CSharpFundamentals.Lesson6;
 
 Task4 task4 = new Task4();
-task4.RemoveElementInList();
+task4.Remove();
+task4.RemoveAt();
+task4.Dequeue();
+task4.Pop();
