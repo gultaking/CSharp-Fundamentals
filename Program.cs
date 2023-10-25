2ac81f866b92d33c88d465369f1919631b9c7814
gultaking <57599732+gultaking@users.noreply.github.com>
2023-10-25 17:15:56 +0400

    CSF-6-4: Remove elements using methods like Remove (#60)
    * CSF-6-4: Remove elements using methods like Remove, RemoveAt, Dequeue, or Pop.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using CSharpFundamentals.Lesson6;
 
-Task5 task5 = new Task5();
-task5.IterationForeachOverDictionary();
-task5.IterationForeachOverList();
+Task4 task4 = new Task4();
+task4.Remove();
+task4.RemoveAt();
+task4.Dequeue();
+task4.Pop();
